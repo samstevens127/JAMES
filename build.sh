08fd7bb5817bd18d37da1a03289214d33ca33529
samstevens127 <89885396+samstevens127@users.noreply.github.com>
2026-01-18 01:04:02 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-# Go to project root
+
 rm -rf build/
 mkdir build && cd build
 uv run cmake -DCMAKE_BUILD_TYPE=Debug ..
 uv run cmake --build . --config Release -j$(nproc)
-# Move the NEWLY built file to your python folder
 cp mcts_cpp.*.so ../src/python/
 cd ..
